58fce30b6180707020ae6046376a7d181521084a
VarunNShenoy <shenoyvarun04@gmail.com>
2026-06-29 12:01:34 -0400

    Add live host detection workflow

--- recon.py
+++ recon.py
@@ -1,4 +1,8 @@
 import subprocess
+import os
+
+os.makedirs("results/subdomains",exist_ok=True)
+os.makedirs("results/live_hosts",exist_ok=True)
 
 domain = input("Enter domain:")
 print(f"Starting recon on {domain}")
@@ -13,19 +17,38 @@ text=True
 subdomains = result.stdout.splitlines()
 print("\nSubdomains Found:\n")
 
-#print each subdomain
-for subdomain in subdomains:
-    print(subdomain)
 
 #save the results to a file
 
-with open(f"results/{domain}.txt", "w") as file:
+with open(f"results/subdomains/{domain}.txt", "w") as file:
   for subdomain in subdomains:
     file.write(subdomain+"\n")
+
+#Run httpx on discovered subdomains
+
+print("\nChecking the live hosts")
+
+httpx_result = subprocess.run(["httpx","-l",f"results/subdomains/{domain}.txt"],
+capture_output=True,
+text=True
+)
+
+#Convert the results into text
+
+live_hosts = httpx_result.stdout.splitlines()
+
+# Save live host in a file
+
+with open(f"results/live_hosts/{domain}_live.txt","w") as file:
+  for host in live_hosts:
+    file.write(host + "\n")
 
 #print scan summary
 print("\nScan Summary")
 print("-"*30)
 print(f"Target: {domain}")
 print(f"Subdomains Found: {len(subdomains)}")
-print(f"Results saved to : results/{domain}.txt")
+print(f"Live Hosts Found:{len(live_hosts)}")
+print(f"Subdomains saved to: results/subdomains/{domain}.txt")
+print(f"Live Domains saved to: results/live_hosts/{domain}_live.txt")
+
